e887546936e36fe52c7d48b5f5f7953cdaf6a65b
test <1010816303@qq.com>
2020-09-11 10:53:13 +0800

    第二次修改

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: 我想再看你一眼");
+        Log.e(TAG, "onCreate: " );
     }
 }
